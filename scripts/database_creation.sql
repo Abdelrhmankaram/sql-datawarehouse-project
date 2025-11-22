9ab4cc2a20931fa377e56ed530c1443eda6c33e3
Abdelrhmankaram <abdelrhmankaram171@gmail.com>
2025-11-22 04:35:07 +0200

    Add initial database creation script for DataWarehouse with bronze, silver, and gold schemas

---
+++ scripts/database_creation.sql
@@ -0,0 +1,15 @@
+-- create database 'data warehouse' --
+
+USE Master;
+GO
+
+Create DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+Create SCHEMA bronze;
+GO
+Create SCHEMA silver;
+GO
+Create SCHEMA gold;
+GO
